a666a265009c9f1e3a21ea3076b7f7b881906a18
jaluarjun442 <160036840+jaluarjun442@users.noreply.github.com>
2025-01-26 15:52:16 +0530

    front minor tag changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@ if (env('APP_ENV') == 'local') {
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>MojHub</title>
-    <meta name="description" content="">
+    <meta name="description" content="MojHub Daily latest video">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="manifest" href="https://devsnews.com/template/bungee/bungee/site.webmanifest/">
